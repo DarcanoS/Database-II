73652bfff8164a388f43354b73e03728293be2ad
Darcano <pinillapuerta111@hotmail.com>
2025-10-20 22:54:51 -0500

    Update:  compilation script to use Report.tex instead of CS_report_template.tex

--- src/Report_Latex/Compilar.sh
+++ src/Report_Latex/Compilar.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Primera compilación para generar archivos auxiliares
-pdflatex -interaction=nonstopmode -shell-escape CS_report_template.tex
+pdflatex -interaction=nonstopmode -shell-escape Report.tex
 
 # Compilar bibliografía
-bibtex CS_report_template
+bibtex Report
 
 # Segunda compilación para procesar las referencias
-pdflatex -interaction=nonstopmode -shell-escape CS_report_template.tex
+pdflatex -interaction=nonstopmode -shell-escape Report.tex
 
 # Tercera compilación para asegurar que todo esté correcto
-pdflatex -interaction=nonstopmode -shell-escape CS_report_template.tex
+pdflatex -interaction=nonstopmode -shell-escape Report.tex
 
 # Crear directorio de destino y mover el PDF
-cp CS_report_template.pdf ../../Catch-Up/Report.pdf
+cp Report.pdf ../../Catch-Up/Report.pdf
 
 echo "Compilación completada. PDF generado en Catch-Up/Report.pdf"
